3e8240982f25dd35ee183a43cd06221f453d2c54
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2022-11-30 01:02:38 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -8,39 +8,74 @@ function displayImages(images) {
   });
 }
 
-function naji () {
-    const image = document.querySelectorAll("#ramen-menu img");
-    image.forEach((img)=>{
-        console.log(img);
-    })
+function clickedImg(images) {
+  const menueImages = document.querySelectorAll("#ramen-menu img");
+  const imgDisplayed = document.querySelector(".detail-image");
+  const nameDisplayed = document.querySelector("h2");
+  const restDisplayed = document.querySelector(".restaurant");
+  const ratingDisplayed = document.getElementById("rating-display");
+  const commentDisplayed = document.getElementById("comment-display");
 
+  images.map((image) => {
+    for (let i = 0; i < menueImages.length; i++) {
+      menueImages[i].addEventListener("click", () => {
+        if (image.id == menueImages[i].id) {
+          console.log(image);
+          console.log(menueImages[i]);
+          console.log("naji");
+          console.log(restDisplayed);
+
+          nameDisplayed.textContent = image.name;
+          restDisplayed.textContent = image.restaurant;
+          imgDisplayed.src = image.image;
+          ratingDisplayed.textContent = image.rating;
+          commentDisplayed.textContent = image.comment;
+        }
+      });
+    }
+  });
 }
-
-// function clickedImg(images) {
-//   //console.log(image);
-//   const imgDisplayed = document.querySelector(".detail-image");
-//   images.map((menueInfo) => {
-//     for (let i = 0; i < image.length; i++) {
-//       image[i].addEventListener("click", () => {
-//         console.log("clicked");
-//         // console.log(image[i]);
-//         if (image[i].id === menueInfo.id) {
-//           console.log("naji");
-//                 imgDisplayed[i].setAttribute("src", `${menueInfo.image}`);
-//                 imgDisplayed[i].setAttribute("name", `${menueInfo.name}`);
-//         }
-//       });
-//     }
-//   });
-// }
 
 fetch("http://localhost:3000/ramens")
   .then((response) => {
     return response.json(); //why we put return to get the data?
   })
   .then((data) => {
-    //console.log(data);
     displayImages(data);
-    //clickedImg(data);
-    naji();
+    clickedImg(data);
   });
+
+const newRamen = document.getElementById("new-ramen")[5];
+newRamen.addEventListener("click", (e) => {
+  e.preventDefault();
+  createNewRamen();
+});
+
+function createNewRamen() {
+  const newName = document.getElementById("new-name");
+  const newRest = document.getElementById("new-restaurant");
+  const newImage = document.getElementById("new-image");
+  const newRating = document.getElementById("new-rating");
+  const newComment = document.getElementById("new-comment");
+
+  fetch("http://localhost:3000/ramens", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      comment: newComment.value,
+      image: newImage.value,
+      name: newName.value,
+      rating: newRating.value,
+      restaurant: newRest.value,
+    }),
+  })
+    .then((response) => {
+      response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+}
